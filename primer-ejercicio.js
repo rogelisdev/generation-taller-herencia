6b38f02409ba1cba6b1d25f51845d6fe89bbc7e3
rogelisdev <rogelisdev@gmail.com>
2026-06-01 15:05:00 -0500

    feat: Metodos ejercicio 1

--- primer-ejercicio.js
+++ primer-ejercicio.js
@@ -9,9 +9,37 @@ class Empleados {
     presentarse() {
         console.log(`Hola mi nombre es ${this.nombre} y mi cargo es el de ${this.cargo}`)
     }
+
+    calcularSalarioAnual() {
+        let calcular = this.salario * 12
+        console.log(`Tu salario anual es de: ${calcular}`)
+        return calcular;
+    }
+
+    desactivar() {
+        if (this.activo === true) {
+            this.activo = false
+            console.log(`El empleado ${this.nombre} su estado es : ${this.activo}`)
+        } else {
+            console.log(`El empleado ${this.nombre} su estado es : ${this.activo}`)
+        }
+
+    }
+
+    info(){
+        console.log(`Datos del trabajador:\n
+        NOMBRE: ${this.nombre}, \n
+        SALARIO: ${this.salario}, \n
+        CARGO: ${this.cargo}, \n
+        ESTADO: ${this.activo}`) 
+    }
+
 }
 
 
 const empleado = new Empleados("Mario", "Albañil", 1200, true);
 
 empleado.presentarse()
+empleado.calcularSalarioAnual()
+empleado.desactivar()
+empleado.info()
